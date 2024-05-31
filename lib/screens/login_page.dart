3890cb905a8c97b35bc8ccab15dc45b4ca998e7d
ecembostancioglu <ecembostancioglu21@gmail.com>
2024-05-31 15:12:11 +0300

    Images of Login Page were added

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned.fill(
+              child: Image.asset('assets/images/background.png',
+              fit: BoxFit.cover)),
+          Align(
+            alignment: Alignment.topCenter,
+            child: Image.asset('assets/images/logo.png',
+            width: 250.w),
+          ),
+        ],
+      ),
+    );
   }
 }
